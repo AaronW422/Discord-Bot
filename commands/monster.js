aa16f2fdb9b494f9976bebe8e2ebb556beed17e0
Aaron Wong <aaronw2291@gmail.com>
2021-09-14 15:57:51 -0400

    Search MHW API with input name

--- commands/monster.js
+++ commands/monster.js
@@ -1,16 +1,22 @@
 const axios = require('axios');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const getData = require('../util/getData.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('monster')
-    .setDescription('Looks up and returns a description of the monster.'),
+    .setDescription('Return a description of the monster.')
+    .addStringOption((option) =>
+      option.setName('input').setDescription('Name of a monster')
+    ),
   async execute(interaction) {
-    const res = await axios.get(
-      `https://mhw-db.com/monsters?q={"name":"anjanath"}`
-    );
-
-    return interaction.reply(res.data[0].name);
+    const input = interaction.options.getString('input');
+    console.log(input);
+    if (input) {
+      const res = await axios.get(
+        `https://mhw-db.com/monsters?q={"name":"${input}"}`
+      );
+      return interaction.reply(res.data[0].name);
+    }
+    return interaction.reply('Please enter the name of a monster!');
   },
 };
