d72a04c07196e8a4f5bf36089660efd3c7faa253
Aaron Wong <aaronw2291@gmail.com>
2021-09-15 15:44:14 -0400

    Create command for MHW items

---
+++ commands/item.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('item')
+    .setDescription('Return a description of the item.')
+    .addStringOption((option) =>
+      option.setName('name').setDescription('Name of a item')
+    ),
+  async execute(interaction) {
+    const name = interaction.options.getString('name');
+    if (name) {
+      const res = await axios.get(
+        `https://mhw-db.com/items?q={"name":"${name}"}`
+      );
+      if (res.data.length) {
+        const item = res.data[0];
+        const itemEmbed = new MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(item.name)
+          .setDescription(item.description)
+          .setURL(
+            `https://monsterhunterworld.wiki.fextralife.com/${item.name
+              .split(' ')
+              .join('+')}`
+          )
+          .addFields(
+            {
+              name: 'Rarity',
+              value: `${item.rarity}`,
+              inline: true,
+            },
+            {
+              name: 'Carry Limit',
+              value: `${item.carryLimit}`,
+              inline: true,
+            },
+            {
+              name: 'Sell Value',
+              value: `${item.value}z`,
+              inline: true,
+            }
+          );
+        return interaction.reply({ embeds: [itemEmbed] });
+      }
+      return interaction.reply(
+        'No item found. Please check the spelling of the name!'
+      );
+    }
+    return interaction.reply('Please enter the name of an item!');
+  },
+};
